9069f8d60d8ca9c11dc17ee7b84b335bcd80f1ae
newcometlab <newcometlab@outlook.com>
2021-09-24 00:36:03 +0900

    Added line chart with annotation

--- src/App.js
+++ src/App.js
@@ -6,21 +6,172 @@ class App extends Component {
     super(props);
 
     this.state = {
+          
+      series: [{
+        data: [
+          8107.85,
+          8128.0,
+          8122.9,
+          8165.5,
+          8340.7,
+          8423.7,
+          8423.5,
+          8514.3,
+          8481.85,
+          8487.7,
+          8506.9,
+          8626.2,
+          8668.95,
+          8602.3,
+          8607.55,
+          8512.9,
+          8496.25,
+          8600.65,
+          8881.1,
+          9340.85
+        ]
+      }],
       options: {
         chart: {
-          id: "basic-line"
+          height: 350,
+          type: 'line',
+          id: 'areachart-2'
         },
+        annotations: {
+          yaxis: [{
+            y: 8200,
+            borderColor: '#00E396',
+            label: {
+              borderColor: '#00E396',
+              style: {
+                color: '#fff',
+                background: '#00E396',
+              },
+              text: 'Support',
+            }
+          }, {
+            y: 8600,
+            y2: 9000,
+            borderColor: '#000',
+            fillColor: '#FEB019',
+            opacity: 0.2,
+            label: {
+              borderColor: '#333',
+              style: {
+                fontSize: '10px',
+                color: '#333',
+                background: '#FEB019',
+              },
+              text: 'Y-axis range',
+            }
+          }],
+          xaxis: [{
+            x: new Date('23 Nov 2017').getTime(),
+            strokeDashArray: 0,
+            borderColor: '#775DD0',
+            label: {
+              borderColor: '#775DD0',
+              style: {
+                color: '#fff',
+                background: '#775DD0',
+              },
+              text: 'Anno Test',
+            }
+          }, {
+            x: new Date('26 Nov 2017').getTime(),
+            x2: new Date('28 Nov 2017').getTime(),
+            fillColor: '#B3F7CA',
+            opacity: 0.4,
+            label: {
+              borderColor: '#B3F7CA',
+              style: {
+                fontSize: '10px',
+                color: '#fff',
+                background: '#00E396',
+              },
+              offsetY: -10,
+              text: 'X-axis range',
+            }
+          }],
+          points: [{
+            x: new Date('01 Dec 2017').getTime(),
+            y: 8607.55,
+            marker: {
+              size: 8,
+              fillColor: '#fff',
+              strokeColor: 'red',
+              radius: 2,
+              cssClass: 'apexcharts-custom-class'
+            },
+            label: {
+              borderColor: '#FF4560',
+              offsetY: 0,
+              style: {
+                color: '#fff',
+                background: '#FF4560',
+              },
+        
+              text: 'Point Annotation',
+            }
+          }, {
+            x: new Date('08 Dec 2017').getTime(),
+            y: 9340.85,
+            marker: {
+              size: 0
+            },
+            image: {
+              path: '../images/ico-instagram.png'
+            }
+          }]
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'straight'
+        },
+        grid: {
+          padding: {
+            right: 30,
+            left: 20
+          }
+        },
+        title: {
+          text: 'Line with Annotations',
+          align: 'left'
+        },
+        labels: [
+          "13 Nov 2017",
+          "14 Nov 2017",
+          "15 Nov 2017",
+          "16 Nov 2017",
+          "17 Nov 2017",
+          "20 Nov 2017",
+          "21 Nov 2017",
+          "22 Nov 2017",
+          "23 Nov 2017",
+          "24 Nov 2017",
+          "27 Nov 2017",
+          "28 Nov 2017",
+          "29 Nov 2017",
+          "30 Nov 2017",
+          "01 Dec 2017",
+          "04 Dec 2017",
+          "05 Dec 2017",
+          "06 Dec 2017",
+          "07 Dec 2017",
+          "08 Dec 2017"
+        ],
         xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-        }
+          type: 'datetime',
+        },
       },
-      series: [
-        {
-          name: "series-1",
-          data: [30, 40, 45, 50, 49, 60, 70, 91]
-        }
-      ]
+    
+    
     };
+
+
+
   }
 
   render() {
