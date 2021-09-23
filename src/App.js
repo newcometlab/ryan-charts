3e33d4a37704161f0a114f82f3e9f66a37fe7f3e
newcometlab <newcometlab@outlook.com>
2021-09-24 05:19:44 +0900

    Updated into Highcharts

--- src/App.js
+++ src/App.js
@@ -1,192 +1,107 @@
 import React, { Component } from "react";
-import Chart from "react-apexcharts";
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
+const options = {
+  title: {
+    text: 'My chart'
+  },
+  series: [{
+    data: [1, 2, 3]
+  }]
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-          
-      series: [{
-        data: [
-          8107.85,
-          8128.0,
-          8122.9,
-          8165.5,
-          8340.7,
-          8423.7,
-          8423.5,
-          8514.3,
-          8481.85,
-          8487.7,
-          8506.9,
-          8626.2,
-          8668.95,
-          8602.3,
-          8607.55,
-          8512.9,
-          8496.25,
-          8600.65,
-          8881.1,
-          9340.85
-        ]
-      }],
-      options: {
-        chart: {
-          height: 350,
-          type: 'line',
-          id: 'areachart-2'
-        },
-        annotations: {
-          yaxis: [{
-            y: 8200,
-            borderColor: '#00E396',
-            label: {
-              borderColor: '#00E396',
-              style: {
-                color: '#fff',
-                background: '#00E396',
-              },
-              text: 'Support',
-            }
-          }, {
-            y: 8600,
-            y2: 9000,
-            borderColor: '#000',
-            fillColor: '#FEB019',
-            opacity: 0.2,
-            label: {
-              borderColor: '#333',
-              style: {
-                fontSize: '10px',
-                color: '#333',
-                background: '#FEB019',
-              },
-              text: 'Y-axis range',
-            }
-          }],
-          xaxis: [{
-            x: new Date('23 Nov 2017').getTime(),
-            strokeDashArray: 0,
-            borderColor: '#775DD0',
-            label: {
-              borderColor: '#775DD0',
-              style: {
-                color: '#fff',
-                background: '#775DD0',
-              },
-              text: 'Anno Test',
-            }
-          }, {
-            x: new Date('26 Nov 2017').getTime(),
-            x2: new Date('28 Nov 2017').getTime(),
-            fillColor: '#B3F7CA',
-            opacity: 0.4,
-            label: {
-              borderColor: '#B3F7CA',
-              style: {
-                fontSize: '10px',
-                color: '#fff',
-                background: '#00E396',
-              },
-              offsetY: -10,
-              text: 'X-axis range',
-            }
-          }],
-          points: [{
-            x: new Date('01 Dec 2017').getTime(),
-            y: 8607.55,
-            marker: {
-              size: 8,
-              fillColor: '#fff',
-              strokeColor: 'red',
-              radius: 2,
-              cssClass: 'apexcharts-custom-class'
-            },
-            label: {
-              borderColor: '#FF4560',
-              offsetY: 0,
-              style: {
-                color: '#fff',
-                background: '#FF4560',
-              },
-        
-              text: 'Point Annotation',
-            }
-          }, {
-            x: new Date('08 Dec 2017').getTime(),
-            y: 9340.85,
-            marker: {
-              size: 0
-            },
-            image: {
-              path: '../images/ico-instagram.png'
-            }
-          }]
-        },
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          curve: 'straight'
-        },
-        grid: {
-          padding: {
-            right: 30,
-            left: 20
-          }
-        },
-        title: {
-          text: 'Line with Annotations',
-          align: 'left'
-        },
-        labels: [
-          "13 Nov 2017",
-          "14 Nov 2017",
-          "15 Nov 2017",
-          "16 Nov 2017",
-          "17 Nov 2017",
-          "20 Nov 2017",
-          "21 Nov 2017",
-          "22 Nov 2017",
-          "23 Nov 2017",
-          "24 Nov 2017",
-          "27 Nov 2017",
-          "28 Nov 2017",
-          "29 Nov 2017",
-          "30 Nov 2017",
-          "01 Dec 2017",
-          "04 Dec 2017",
-          "05 Dec 2017",
-          "06 Dec 2017",
-          "07 Dec 2017",
-          "08 Dec 2017"
-        ],
-        xaxis: {
-          type: 'datetime',
-        },
+    this.seriesData = [
+      {
+        date: "2020-10-21",
+        score: 0,
+        note: ""
       },
-    
-    
-    };
-
-
+      {
+        date: "2020-10-22",
+        score: 10,
+        note: "positive event established"
+      },
+      {
+        date: "2020-10-23",
+        score: 0,
+        note: ""
+      },
+      {
+        date: "2020-10-24",
+        score: -1,
+        note: "some other event"
+      },
+      {
+        date: "2020-10-25",
+        score: -5,
+        note: "some other event"
+      },
+      {
+        date: "2020-10-26",
+        score: 8,
+        note: "positive event established"
+      },
+      {
+        date: "2020-10-27",
+        score: 0,
+        note: ""
+      },
+      {
+        date: "2020-10-28",
+        score: -10,
+        note: "critical event established"
+      },
+      {
+        date: "2020-10-29",
+        score: 0,
+        note: ""
+      },
+      {
+        date: "2020-10-30",
+        score: 0,
+        note: ""
+      },
+      {
+        date: "2020-10-31",
+        score: 6,
+        note: "some other event"
+      },
+      {
+        date: "2020-11-01",
+        score: 0,
+        note: ""
+      },
+      {
+        date: "2020-11-02",
+        score: 0,
+        note: ""
+      },
+      {
+        date: "2020-11-03",
+        score: -5,
+        note: "some other event"
+      },
+      {
+        date: "2020-11-04",
+        score: 0,
+        note: ""
+      },
+    ];
 
   }
 
   render() {
     return (
       <div className="app">
-        <div className="row">
-          <div className="mixed-chart">
-            <Chart
-              options={this.state.options}
-              series={this.state.series}
-              type="line"
-              width="500"
-            />
-            </div>
-        </div>
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={options}
+        />
       </div>
     );
   }
