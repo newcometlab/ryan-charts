8943afbf4951f5ba070baf54ac34f1d71965931d
newcometlab <newcometlab@outlook.com>
2021-09-24 13:01:59 +0900

    Finished gauge chart

--- src/RadialChart.js
+++ src/RadialChart.js
@@ -13,82 +13,105 @@ class RadialChart extends Component {
     this.chartOptions = {
         chart: {
             type: 'solidgauge',
-        },
-    
-        title: {
-            text: 'Activity',
-            style: {
-                fontSize: '24px'
+            plotBackgroundImage: '../images/man.png'
+          },
+          title: null,
+          pane: {
+            size: '80%',
+            center: ["50%", "50%"],
+            startAngle: -130,
+            endAngle: 130,
+            background: {
+              borderWidth: 54,
+              backgroundColor: '#8b8ca5',
+              shape: 'arc',
+              borderColor: '#8b8ca5',
+              outerRadius: '85%',
+              innerRadius: '85%'
             }
-        },
-    
-        tooltip: {
-            borderWidth: 0,
-            backgroundColor: 'none',
-            shadow: false,
-            style: {
-                fontSize: '16px'
-            },
-            valueSuffix: '%',
-            pointFormat: '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}</span>',
-            positioner: function (labelWidth) {
-                return {
-                    x: (this.chart.chartWidth - labelWidth) / 2,
-                    y: (this.chart.plotHeight / 2) + 15
-                };
-            }
-        },
-    
-        pane: {
-            startAngle: 0,
-            endAngle: 360,
-            background: [{ // Track for Move
-                outerRadius: '112%',
-                innerRadius: '88%',
-                backgroundColor: Highcharts.color(Highcharts.getOptions().colors[0])
-                    .setOpacity(0.3)
-                    .get(),
-                borderWidth: 0
-            }]
-        },
-    
-        yAxis: {
+          },
+          yAxis: {
             min: 0,
             max: 100,
             lineWidth: 0,
-            tickPositions: []
-        },
-    
-        plotOptions: {
+            tickPositions: [],
+            stops: [
+              [0.1, '#fe5d54'],
+              [0.9, '#ff9700']
+            ],
+          },
+          tooltip: {
+            enabled: false
+          },
+          plotOptions: {
             solidgauge: {
-                dataLabels: {
-                    enabled: false
-                },
-                linecap: 'round',
-                stickyTracking: false,
-                rounded: true
+              borderWidth: 0,
+              radius: '95%',
+              innerRadius: '75%',
+              linecap: 'round',
+              rounded: true,
+              dataLabels: {
+                y: 100,
+                borderWidth: 0,
+                useHTML: true
+              }
             }
-        },
-    
-        series: [{
-            name: 'Move',
-            data: [{
-                color: Highcharts.getOptions().colors[0],
-                radius: '112%',
-                innerRadius: '88%',
-                y: 80
-            }]
-        }]
+          },
+          credits: {
+              enabled:false
+          },
+          series: [{
+            name: 'overall wellbeing',
+            data: [70],
+            dataLabels: {
+              format: '<div style="text-align:center;font-size:30px;color:#333951;line-height: 1.1;"><div style="text-align:center;">Overall</div style="text-align:center;"><div>Wellbeing</div></div>'
+            }
+          }]
     }
+
+    this.chartCallback = (chart) => {
+        var y = chart.series[0].data[0].y;
+        for (var i = y; i >= 0; i = i - (y / 80)) {
+          chart.addSeries({
+            data: [{
+              y: i,
+            }],
+            stickyTracking: false,
+            enableMouseTracking: false
+          }, false)
+        }
+        chart.redraw();
+        Highcharts.each(chart.series, function(s) {
+          s.update({
+            borderColor: s.data[0].color
+          }, false);
+        });
+        chart.redraw();
+      }
 
   }
 
+    componentDidMount() {
+        console.log(document);
+        let svg = document.getElementsByTagName('svg');
+        if (svg.length > 0) {
+          let path = svg[0].getElementsByTagName('path');
+          if (path.length > 1) {
+            path[0].setAttributeNS(null, 'stroke-linejoin', 'round');
+          }
+        }
+    }
+
   render() {
     return (
-        <HighchartsReact
-          highcharts={Highcharts}
-          options={this.chartOptions}
-        />
+        <div
+        style={{width:"400px", height:"400px", margin: "0 auto"}}>            
+            <HighchartsReact
+              highcharts={Highcharts}
+              options={this.chartOptions}
+              callback={this.chartCallback}
+            />
+        </div>
     );
   }
 }
