2a645570484caf35a8ea7324ccc3b9331ecfa4d8
newcometlab <newcometlab@outlook.com>
2021-09-25 09:34:28 +0900

    Fixed image size

--- src/LineChart.js
+++ src/LineChart.js
@@ -3,207 +3,206 @@ import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 
 class LineChart extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.rawData = [
-      {
-        date: "2020-10-20",
-        score: 0,
-        note: ""
-      },
-      {
-        date: "2020-10-21",
-        score: 0,
-        note: ""
-      },
-      {
-        date: "2020-10-22",
-        score: 10,
-        note: "positive event established"
-      },
-      {
-        date: "2020-10-23",
-        score: 0,
-        note: ""
-      },
-      {
-        date: "2020-10-24",
-        score: -1,
-        note: "some other event"
-      },
-      {
-        date: "2020-10-25",
-        score: -5,
-        note: "some other event"
-      },
-      {
-        date: "2020-10-26",
-        score: 8,
-        note: "positive event established"
-      },
-      {
-        date: "2020-10-27",
-        score: 0,
-        note: ""
-      },
-      {
-        date: "2020-10-28",
-        score: -10,
-        note: "critical event established"
-      },
-      {
-        date: "2020-10-29",
-        score: 0,
-        note: ""
-      },
-      {
-        date: "2020-10-30",
-        score: 0,
-        note: ""
-      },
-      {
-        date: "2020-10-31",
-        score: 6,
-        note: "some other event"
-      },
-      {
-        date: "2020-11-01",
-        score: 0,
-        note: ""
-      },
-      {
-        date: "2020-11-02",
-        score: 0,
-        note: ""
-      },
-      {
-        date: "2020-11-03",
-        score: -5,
-        note: "some other event"
-      },
-      {
-        date: "2020-11-04",
-        score: 0,
-        note: ""
-      },
-    ];
+		this.rawData = [
+			{
+				date: "2020-10-20",
+				score: 0,
+				note: ""
+			},
+			{
+				date: "2020-10-21",
+				score: 0,
+				note: ""
+			},
+			{
+				date: "2020-10-22",
+				score: 10,
+				note: "positive event established"
+			},
+			{
+				date: "2020-10-23",
+				score: 0,
+				note: ""
+			},
+			{
+				date: "2020-10-24",
+				score: -1,
+				note: "some other event"
+			},
+			{
+				date: "2020-10-25",
+				score: -5,
+				note: "some other event"
+			},
+			{
+				date: "2020-10-26",
+				score: 8,
+				note: "positive event established"
+			},
+			{
+				date: "2020-10-27",
+				score: 0,
+				note: ""
+			},
+			{
+				date: "2020-10-28",
+				score: -10,
+				note: "critical event established"
+			},
+			{
+				date: "2020-10-29",
+				score: 0,
+				note: ""
+			},
+			{
+				date: "2020-10-30",
+				score: 0,
+				note: ""
+			},
+			{
+				date: "2020-10-31",
+				score: 6,
+				note: "some other event"
+			},
+			{
+				date: "2020-11-01",
+				score: 0,
+				note: ""
+			},
+			{
+				date: "2020-11-02",
+				score: 0,
+				note: ""
+			},
+			{
+				date: "2020-11-03",
+				score: -5,
+				note: "some other event"
+			},
+			{
+				date: "2020-11-04",
+				score: 0,
+				note: ""
+			},
+		];
 
-    this.seriesData = this.rawData.map(one => {
-      return {
-        y: one.score,
-        x: Date.UTC(
-          parseInt(one.date.substring(0, 4)),
-          parseInt(one.date.substring(5, 7)) - 1,
-          parseInt(one.date.substring(8, 10))
-        ),
-        marker: {
-          enabled: one.note !== "" ? true : false
-        },
-        name: one.note
-      };
-    });
-    this.chartOptions = {
-      chart: {
-        height: 200,
-        type: 'spline',
-        events: {
-          load: function() {
-            var chart = this;
-            chart.update({
-              plotOptions: {
-                series: {
-                  color: {
-                    linearGradient: [0, "-10%", 0, '90%']
-                  }
-                }
-              }
-            });
-          }
-        }
-      },
-      title: {
-        text: ''
-      },
-      credits: {
-        enabled: false
-      },
-      legend: {
-        enabled: false
-      },
-      yAxis: {
-        title: {
-          text: ''
-        },
-        min: -10,
-        max: 10
-      },
-      xAxis: {
-        type: 'datetime',
-        labels: {
-          formatter: function() {
-            return Highcharts.dateFormat('%e', this.value);
-          }
-        },
-      },
-      plotOptions: {
-        spline: {
-          lineWidth: 5,
-          marker: {
-            lineColor: '#fff',
-            lineWidth: 3,
-            radius: 6
-          },
-        },
-        series: {
-          color: {
-            linearGradient: [0, 0, 0, 0],
-            stops: [
-              [0.00, '#57e50b'],
-              [0.4, '#defe66'],
-              [0.5, '#d7d8ff'],
-              [0.6, '#ffdb6d'],
-              [1.00, '#ff0000']
-            ]
-          }
+		this.seriesData = this.rawData.map(one => {
+			return {
+				y: one.score,
+				x: Date.UTC(
+					parseInt(one.date.substring(0, 4)),
+					parseInt(one.date.substring(5, 7)) - 1,
+					parseInt(one.date.substring(8, 10))
+				),
+				marker: {
+					enabled: one.note !== "" ? true : false
+				},
+				name: one.note
+			};
+		});
+		this.chartOptions = {
+			chart: {
+				height: 200,
+				type: 'spline',
+				events: {
+					load: function() {
+						var chart = this;
+						chart.update({
+							plotOptions: {
+								series: {
+									color: {
+										linearGradient: [0, "-10%", 0, '90%']
+									}
+								}
+							}
+						});
+					}
+				}
+			},
+			title: {
+				text: ''
+			},
+			credits: {
+				enabled: false
+			},
+			legend: {
+				enabled: false
+			},
+			yAxis: {
+				title: {
+					text: ''
+				},
+				min: -10,
+				max: 10
+			},
+			xAxis: {
+				type: 'datetime',
+				labels: {
+					formatter: function() {
+						return Highcharts.dateFormat('%e', this.value);
+					}
+				},
+			},
+			plotOptions: {
+				spline: {
+					lineWidth: 5,
+					marker: {
+						lineColor: '#fff',
+						lineWidth: 3,
+						radius: 6
+					},
+				},
+				series: {
+					color: {
+						linearGradient: [0, 0, 0, 0],
+						stops: [
+							[0.00, '#57e50b'],
+							[0.4, '#defe66'],
+							[0.5, '#d7d8ff'],
+							[0.6, '#ffdb6d'],
+							[1.00, '#ff0000']
+						]
+					}
 
-        }
-      },
-      tooltip: {
-        backgroundColor: "transparent",
-        borderWidth: 0,
-        borderRadius: 0,
-        shadow: false,
-        padding: 0,
-        style: {
-          color: "#fff"
-        },
-        formatter: function() {
-          var point_date = new Date(this.point.x);
-          var date_str = point_date.toLocaleString('default', {month: 'long'})
-                         + " " + point_date.toLocaleString('default', {day: 'numeric'})
-                         + "th, " + point_date.toLocaleString('default', {year: 'numeric'});
-          return '<div class="bg-tooltip">'
-                + '<div style="font-size:14px;">' + date_str + '</div>' 
-                + '<div style="font-size:12px;">' +  this.point.name + '</div>' + '</div>';
-        },
-        useHTML: true
-      },  
-      series: [{
-        data: this.seriesData
-      }]
-    }
+				}
+			},
+			tooltip: {
+				backgroundColor: "transparent",
+				borderWidth: 0,
+				borderRadius: 0,
+				shadow: false,
+				padding: 0,
+				style: {
+					color: "#fff"
+				},
+				formatter: function() {
+					var point_date = new Date(this.point.x);
+					var date_str = point_date.toLocaleString('default', {month: 'long'})
+												 + " " + point_date.toLocaleString('default', {day: 'numeric'})
+												 + "th, " + point_date.toLocaleString('default', {year: 'numeric'});
+					return '<div class="bg-tooltip">'
+								+ '<div style="font-size:14px;">' + date_str + '</div>' 
+								+ '<div style="font-size:12px;">' +  this.point.name + '</div>' + '</div>';
+				},
+				useHTML: true
+			},  
+			series: [{
+				data: this.seriesData
+			}]
+		}
+	}
 
-  }
-
-  render() {
-    return (
-        <HighchartsReact
-          highcharts={Highcharts}
-          options={this.chartOptions}
-        />
-    );
-  }
+	render() {
+		return (
+			<HighchartsReact
+				highcharts={Highcharts}
+				options={this.chartOptions}
+			/>
+		);
+	}
 }
 
 export default LineChart;
--- src/RadialChart.js
+++ src/RadialChart.js
@@ -7,155 +7,154 @@ highchartsMore(Highcharts);
 SolidGaugeChart(Highcharts);
 
 class RadialChart extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.seriesData = 67;
-    let chartImage = null;
-    let chartLabel1 = null;
-    let chartLabel2 = null;
-    this.chartOptions = {
-        chart: {
-            type: 'solidgauge',
-            backgroundColor: 'transparent',
-            margin: 0,
-            spacing: 0,
-            events: {
-              load: function () {
-                let plot_scale = this.plotWidth > this.plotHeight ? this.plotHeight : this.plotWidth;
-                chartImage = this.renderer.image('../images/man.png', (this.plotWidth - plot_scale) / 2, (this.plotHeight - plot_scale) / 2, plot_scale, plot_scale).add();
-                chartLabel1 = this.renderer.text('Overall', (this.plotWidth / 2 - plot_scale * 0.14), (this.plotHeight + plot_scale * 0.7) / 2)
-                  .css({
-                    fontSize: plot_scale * 0.08 + 'px',
-                    color: '#333951',
-                    fontWeight: '900'
-                  })
-                  .add();
-                chartLabel2 = this.renderer.text('Wellbeing', (this.plotWidth / 2 - plot_scale * 0.19), (this.plotHeight + plot_scale * 0.7) / 2 + plot_scale * 0.1)
-                  .css({
-                    fontSize: plot_scale * 0.08 + 'px',
-                    color: '#333951',
-                    fontWeight: '900'
-                  })
-                  .add();
-              },
-              redraw: function() {
-                if (chartImage !== null) {
-                  let plot_scale = this.plotWidth > this.plotHeight ? this.plotHeight : this.plotWidth;
-                  chartImage.destroy();
-                  chartImage = this.renderer.image('../images/man.png', (this.plotWidth - plot_scale) / 2, (this.plotHeight - plot_scale) / 2, plot_scale, plot_scale).add();
-                  chartLabel1.destroy();
-                  chartLabel1 = this.renderer.text('Overall', (this.plotWidth / 2 - plot_scale * 0.14), (this.plotHeight + plot_scale * 0.7) / 2)
-                    .css({
-                      fontSize: plot_scale * 0.08 + 'px',
-                      color: '#333951',
-                      fontWeight: '900'
-                    })
-                    .add();
-                  chartLabel2.destroy();
-                  chartLabel2 = this.renderer.text('Wellbeing', (this.plotWidth / 2 - plot_scale * 0.19), (this.plotHeight + plot_scale * 0.7) / 2 + plot_scale * 0.1)
-                    .css({
-                      fontSize: plot_scale * 0.08 + 'px',
-                      color: '#333951',
-                      fontWeight: '900'
-                    })
-                    .add();
-                }
-              }
-            }
-          },
-          title: null,
-          pane: {
-            size: '80%',
-            center: ["50%", "50%"],
-            startAngle: -130,
-            endAngle: 130,
-            background: {
-              borderWidth: "12%",
-              backgroundColor: '#8b8ca5',
-              shape: 'arc',
-              borderColor: '#8b8ca5',
-              outerRadius: '85%',
-              innerRadius: '85%'
-            }
-          },
-          yAxis: {
-            min: 0,
-            max: 100,
-            lineWidth: 0,
-            tickPositions: [],
-            stops: [
-              [0.1, '#fe5d54'],
-              [0.9, '#ff9700']
-            ],
-          },
-          tooltip: {
-            enabled: false
-          },
-          plotOptions: {
-            solidgauge: {
-              borderWidth: 0,
-              radius: '95%',
-              innerRadius: '75%',
-              linecap: 'round',
-              rounded: true,
-              dataLabels: {
-                enabled: false
-              }
-            }
-          },
-          credits: {
-              enabled:false
-          },
-          series: [{
-            name: 'overall wellbeing',
-            data: [this.seriesData]
-          }]
-    }
+		this.rawData = 67;
+		let chartImage = null;
+		let chartLabel1 = null;
+		let chartLabel2 = null;
+		this.chartOptions = {
+			chart: {
+				type: 'solidgauge',
+				backgroundColor: 'transparent',
+				margin: 0,
+				spacing: 0,
+				events: {
+					load: function () {
+						let plot_scale = this.plotWidth > this.plotHeight ? this.plotHeight : this.plotWidth;
+						chartImage = this.renderer.image('../images/man.png', (this.plotWidth - plot_scale * 0.5) / 2, (this.plotHeight - plot_scale * 0.5) / 2, plot_scale * 0.5, plot_scale * 0.5).add();
+						chartLabel1 = this.renderer.text('Overall', (this.plotWidth / 2 - plot_scale * 0.14), (this.plotHeight + plot_scale * 0.7) / 2)
+							.css({
+								fontSize: plot_scale * 0.08 + 'px',
+								color: '#333951',
+								fontWeight: '900'
+							})
+							.add();
+						chartLabel2 = this.renderer.text('Wellbeing', (this.plotWidth / 2 - plot_scale * 0.19), (this.plotHeight + plot_scale * 0.7) / 2 + plot_scale * 0.1)
+							.css({
+								fontSize: plot_scale * 0.08 + 'px',
+								color: '#333951',
+								fontWeight: '900'
+							})
+							.add();
+					},
+					redraw: function() {
+						if (chartImage !== null) {
+							let plot_scale = this.plotWidth > this.plotHeight ? this.plotHeight : this.plotWidth;
+							chartImage.destroy();
+							chartImage = this.renderer.image('../images/man.png', (this.plotWidth - plot_scale * 0.5) / 2, (this.plotHeight - plot_scale * 0.5) / 2, plot_scale * 0.5, plot_scale * 0.5).add();
+							chartLabel1.destroy();
+							chartLabel1 = this.renderer.text('Overall', (this.plotWidth / 2 - plot_scale * 0.14), (this.plotHeight + plot_scale * 0.7) / 2)
+								.css({
+									fontSize: plot_scale * 0.08 + 'px',
+									color: '#333951',
+									fontWeight: '900'
+								})
+								.add();
+							chartLabel2.destroy();
+							chartLabel2 = this.renderer.text('Wellbeing', (this.plotWidth / 2 - plot_scale * 0.19), (this.plotHeight + plot_scale * 0.7) / 2 + plot_scale * 0.1)
+								.css({
+									fontSize: plot_scale * 0.08 + 'px',
+									color: '#333951',
+									fontWeight: '900'
+								})
+								.add();
+						}
+					}
+				}
+			},
+			title: null,
+			pane: {
+				size: '80%',
+				center: ["50%", "50%"],
+				startAngle: -130,
+				endAngle: 130,
+				background: {
+					borderWidth: "11%",
+					backgroundColor: '#8b8ca5',
+					shape: 'arc',
+					borderColor: '#8b8ca5',
+					outerRadius: '85%',
+					innerRadius: '85%'
+				}
+			},
+			yAxis: {
+				min: 0,
+				max: 100,
+				lineWidth: 0,
+				tickPositions: [],
+				stops: [
+					[0.1, '#fe5d54'],
+					[0.9, '#ff9700']
+				],
+			},
+			tooltip: {
+				enabled: false
+			},
+			plotOptions: {
+				solidgauge: {
+					borderWidth: 0,
+					radius: '94%',
+					innerRadius: '76%',
+					linecap: 'round',
+					rounded: true,
+					dataLabels: {
+						enabled: false
+					}
+				}
+			},
+			credits: {
+				enabled:false
+			},
+			series: [{
+				name: 'overall wellbeing',
+				data: [this.rawData]
+			}]
+		}
 
-    this.chartCallback = (chart) => {
-        var y = chart.series[0].data[0].y;
-        for (var i = y; i >= 0; i = i - (y / 80)) {
-          chart.addSeries({
-            data: [{
-              y: i,
-            }],
-            stickyTracking: false,
-            enableMouseTracking: false
-          }, false)
-        }
-        chart.redraw();
-        Highcharts.each(chart.series, function(s) {
-          s.update({
-            borderColor: s.data[0].color
-          }, false);
-        });
-        chart.redraw();
-      }
+		this.chartCallback = (chart) => {
+			var y = chart.series[0].data[0].y;
+			for (var i = y; i >= 0; i = i - (y / 80)) {
+				chart.addSeries({
+					data: [{
+						y: i,
+					}],
+					stickyTracking: false,
+					enableMouseTracking: false
+				}, false)
+			}
+			chart.redraw();
+			Highcharts.each(chart.series, function(s) {
+				s.update({
+					borderColor: s.data[0].color
+				}, false);
+			});
+			chart.redraw();
+		}
+	}
 
-  }
+	componentDidMount() {
+		let svg = document.getElementsByTagName('svg');
+		if (svg.length > 0) {
+			let path = svg[0].getElementsByTagName('path');
+			if (path.length > 1) {
+				path[0].setAttributeNS(null, 'stroke-linejoin', 'round');
+			}
+		}
+	}
 
-    componentDidMount() {
-        let svg = document.getElementsByTagName('svg');
-        if (svg.length > 0) {
-          let path = svg[0].getElementsByTagName('path');
-          if (path.length > 1) {
-            path[0].setAttributeNS(null, 'stroke-linejoin', 'round');
-          }
-        }
-    }
-
-  render() {
-    return (
-        <div className="radial-chart">            
-            <HighchartsReact
-              highcharts={Highcharts}
-              options={this.chartOptions}
-              callback={this.chartCallback}
-            />
-        </div>
-    );
-  }
+	render() {
+		return (
+			<div className="radial-chart">
+				<HighchartsReact
+					highcharts={Highcharts}
+					options={this.chartOptions}
+					callback={this.chartCallback}
+				/>
+			</div>
+		);
+	}
 }
 
 export default RadialChart;
